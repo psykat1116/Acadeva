5462671315439ab1ca6dc16c7c40200a9b21271d
Saikat Samanta <95569573+psykat1116@users.noreply.github.com>
2024-05-29 15:43:27 +0530

    Update Background & Color Scheme

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full flex items-center justify-center">
+    <div className="h-full flex items-center justify-center bg-[url('/background.jpg')] bg-cover bg-center bg-no-repeat">
+      <div className="h-full w-full bg-[rgba(0,0,0,0.4)] absolute top-0 left-0" />
       {children}
     </div>
   );
